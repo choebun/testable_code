cf7487f6cb4ab55934d0644eab3206d1a5f91d24
Jerry He <jerry_he@trendmicro.com>
2020-02-19 13:47:28 +0800

    jerry added

--- AccountingWeb/Controllers/testingController.cs
+++ AccountingWeb/Controllers/testingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccountingWeb.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,25 @@ namespace AccountingWeb.Controllers
         // GET: testing
         public ActionResult testing()
         {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Result(string year, string month, string budget)
+        {
+
+            ViewBag.Message = "Your result page.";
+            ViewBag.Month = month;
+            ViewBag.Year = year;
+            ViewBag.Budget = budget;
+
+            using (var dbContext = new AccountingEntitiesProd())
+            {
+                dbContext.Budgets.Add(new Budget() { Amount = System.Convert.ToDecimal(budget), YearMonth = year + month });
+                dbContext.SaveChanges();
+            }
+
             return View();
         }
     }
