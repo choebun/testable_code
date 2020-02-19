bd6636ef00a2a33b4adef4169cb04b5c2bbfb7f9
ethan hsu <ethan_hsu@trendmicro.com>
2020-02-19 12:23:40 +0800

    haha

---
+++ AccountingWeb/Controllers/budgetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountingWeb.Controllers
+{
+    public class budgetController : Controller
+    {
+        // GET: budget
+        public ActionResult create()
+        {
+            return View();
+        }
+    }
+}
---
+++ AccountingWebTests/CheckEnvironment/UiTest.cs
@@ -0,0 +1,38 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace AccountingWebTests.CheckEnvironment
+{
+    [TestFixture]
+    public class UiTest
+    {
+        //if you cannot run web test, please check if you use the Chrome version 80 browser.
+        //if your version is less than 80, you may need to downgrade nuget package "Selenium.WebDriver.ChromeDriver" version to the appropriate version
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Find information about AtataContext set-up on https://atata.io/getting-started/#set-up
+            AtataContext.Configure()
+                        .UseChrome()
+                        //    WithArguments("start-maximized").
+                        //.UseBaseUrl("http://automationpractice.com/index.php")
+                        .UseCulture("en-us").UseNUnitTestName()
+                        .AddNUnitTestContextLogging().LogNUnitError()
+                        .UseAssertionExceptionType<NUnit.Framework.AssertionException>()
+                        .UseNUnitAggregateAssertionStrategy().Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+
+        [Test]
+        public void go_to_joey_blog()
+        {
+            Go.ToUrl("https://dotblogs.com.tw/hatelove/1");
+        }
+    }
+}
